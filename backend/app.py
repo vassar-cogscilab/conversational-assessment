e2cc15a1c0429495337e90470fae1d99fe76fddf
Toby Fantl <tobyfantl@gmail.com>
2026-06-12 13:57:57 -0400

    fixing api key

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,8 @@ import json
 
 ##load_dotenv(dotenv_path="local.env")
 
-load_dotenv()
+load_dotenv(dotenv_path="/home/ec2-user/convo-api/.env")
+api_key = os.environ.get("ANTHROPIC_API_KEY")
 
 app = Flask(__name__)
 
